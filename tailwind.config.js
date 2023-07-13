8af8f14c4f2f05ac0cb236fbbc6a41cd4808a33c
Kurapika <victorakhihiero@gmail.com>
2023-07-13 20:45:18 +0100

    initial commit with rough sketch files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    fontSize: {},
+    extend: {
+      colors: {
+        "midnight-blue": "#071629",
+        "school-purple": "#C271FF;",
+      },
+    },
+  },
+  plugins: [],
+};
